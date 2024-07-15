5ce356fbadace23c74ba9a5c4886db9584173dfb
ArelShatz <93481267+ArelShatz@users.noreply.github.com>
2024-07-15 14:43:11 +0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -7,8 +7,11 @@ const { generateAlert } = require('./alertGenerator');
 const postgres = require('./postgres-config');
 const { getIdsXml } = require('./xml-parser');
 const cors = require('cors');
+import fetch from 'node-fetch';
 
 let currentIds: Number[] = [];
+
+const AuthUrl = '';
 
 const getIdsFromCap = () => {
     currentIds = getIdsXml();
@@ -24,16 +27,27 @@ app.get('/poligon', async (req: Request, res: Response) => {
     await postgres.query('SELECT * FROM ');
 })
 
-//setInterval(() => getIdsFromCap(), 1000);
+setInterval(() => getIdsFromCap(), 1000);
 
-ioServer.use((socket: Socket, next: any) => {
+ioServer.use(async (socket: Socket, next: any) => {
     const clientId = socket.handshake.query.clientId;
     const clientSecret = socket.handshake.query.clientSecret;
-    next();
+    const body = JSON.stringify({ clientId, clientSecret });
+
+    const response = await fetch(AuthUrl, {
+        method: 'POST',
+        body,
+    });
+
+    if (response.ok) {
+        next();
+    } else {
+        next(new Error('token authentication failed'));
+    }
 });
 
-const sendAlert = (socket: Socket) => {
-    socket.to('alerts').emit('alert', generateAlert());
+const sendAlert = () => {
+    ioServer.to('alerts').emit('alert', generateAlert());
 }
 
 ioServer.on('connection', (socket: Socket) => {
@@ -42,10 +56,10 @@ ioServer.on('connection', (socket: Socket) => {
         if (room === 'alerts') {
             socket.join(room);
 
-            setInterval(() => sendAlert(socket), 3000);
+            setInterval(() => sendAlert(), 3000);
 
             socket.on('ids-request', () => {
-                socket.to('alerts').emit('ids', currentIds);
+                ioServer.to('alerts').emit('ids', currentIds);
             })
         }
     })
--- xml-parser.ts
+++ xml-parser.ts
@@ -1,7 +1,10 @@
 import { XMLParser } from 'fast-xml-parser';
+import { readFileSync } from 'fs';
 
-export const getIdsXml = (capString: string) => {
+export const getIdsXml = () => {
+    const xmlFile = readFileSync(`./test.xml`, 'utf8');
     const parser = new XMLParser();
-    const xmlDoc = parser.parse(capString);
-    return xmlDoc['ns3:value'];
+    const xmlDoc = parser.parse(xmlFile);
+    const geoIds = xmlDoc['env:Envelope']['env:Body']['ns3:alert']['ns3:info']['ns3:area']['ns3:geocode'][1]['ns3:value'];
+    return geoIds.split(',').map((geoId: string) => Number(geoId));
 }
